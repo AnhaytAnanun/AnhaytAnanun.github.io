d482e205845611ee79f9cf914b9dd33f2217a34e
anhaytananun <anhaytananun@gmail.com>
2019-11-07 21:17:51 -0600

    final version 1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 (function($){
   $(function(){
-
     $('.sidenav').sidenav();
-
+    $('.modal').modal();
+    $('#textarea1').val('New Text');
+    $('input#input_text, textarea#textarea2').characterCounter();
+	M.textareaAutoResize($('#textarea1'));
   });
 })(jQuery);
